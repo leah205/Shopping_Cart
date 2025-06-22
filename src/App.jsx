aa64266a0e80b0b77ab69dca5fefd9782092dc80
leah205 <leahtiktin@gmail.com>
2025-06-21 21:26:02 -0600

    style nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import { useState } from 'react'
 import {Outlet} from "react-router-dom"
-import './App.css'
+
 import React from "react"
 import Nav from './components/Nav'
+import {createGlobalStyle} from 'styled-components'
 
+
+
+const GlobalStyles = createGlobalStyle`
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box
+}
+
+#root{
+
+height: 100vh
+}
+
+
+
+
+`;
 function App() {
   
 
   return (
     <>
+    <GlobalStyles />
      <Nav />
       <Outlet />
-    </>
+   
+    
+     </>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const StyledNav = styled.nav`
+    display: flex;
+    list-style-type: none;
+    text-decoration: none;
+    width: 100vw;
+    background: navy;
+`
+
+const NavList = styled.ul`
+    list-style-type: none;
+    display: flex;
+`
+const StyledLink = styled.li`
+    &:hover{
+    background: white;
+}
+    height: 100%;
+    padding: 20px;
+
+`
+
 
 export default function Nav(){
-    return <>
-        <nav>
-            <ul>
-                <li><a href="home">Home</a></li>
-                <li><a href="shop">Shop</a></li>
-            </ul>
+    return (
+        <StyledNav>
+            <NavList>
+                <StyledLink><Link to = "home">Home</Link></StyledLink>
+                <StyledLink><Link to = "shop">Shop </Link></StyledLink>
+            </NavList>
             
-        </nav>
+        </StyledNav> )
     
-    </>
+    
 }
 
