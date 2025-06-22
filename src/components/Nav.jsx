80939bf4a706d20a8fcf36a408765d775f6c8374
leah205 <leahtiktin@gmail.com>
2025-06-21 20:54:09 -0600

    add routing

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Nav(){
+    return <>
+        <nav>
+            <ul>
+                <li><a href="home">Home</a></li>
+                <li><a href="shop">Shop</a></li>
+            </ul>
+            
+        </nav>
+    
+    </>
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Shop from './components/Shop.jsx'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true,
+        element: <Home />
+      },
+      {
+        path: "shop",
+        element:  <Shop />
+      },
+      {
+      path: "home",
+      element: <Home />
+      }
+    ]
+  }
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
