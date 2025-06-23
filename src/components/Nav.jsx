75006595eda813a55f4809fc139af1ed59a70e69
leah205 <leahtiktin@gmail.com>
2025-06-23 10:24:56 -0600

    add cart to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,6 +13,12 @@ const StyledNav = styled.nav`
 const NavList = styled.ul`
     list-style-type: none;
     display: flex;
+    justify-content:space-between;
+    width:100%;
+    div{
+    display:flex
+    }
+    
 `
 const StyledLink = styled.li`
     &:hover{
@@ -20,7 +26,12 @@ const StyledLink = styled.li`
 }
     height: 100%;
     padding: 20px;
+    color: rgb(173, 173, 255);
+`
 
+const CartButton = styled.button`
+    background: none;
+    border: 0;
 `
 
 
@@ -28,8 +39,12 @@ export default function Nav(){
     return (
         <StyledNav>
             <NavList>
+                <div>
                 <StyledLink><Link to = "home">Home</Link></StyledLink>
                 <StyledLink><Link to = "shop">Shop </Link></StyledLink>
+                </div>
+                <StyledLink><CartButton>Cart</CartButton></StyledLink>
+
             </NavList>
             
         </StyledNav> )
