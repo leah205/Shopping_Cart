a3fbcea3009237a462b86c4c415d6c625b8a4dab
leah205 <leahtiktin@gmail.com>
2025-06-23 10:04:07 -0600

    set up basic home page

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    background: blue;
+    color: white;
+    padding: 10px 20px;
+    font-size: 1.2rem;
+    border-radius: 5px;
+    width: fit-content;
+    margin: auto;
+    
+    display: block;
+`
+export default Button
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,49 @@
+import styled from "styled-components"
+import storeSrc from "../assets/store.jpg"
+import Button from "./Button"
+import {Link} from "react-router-dom"
+
+/*let StoreImg = styled.img`
+    width: 60vw;
+    margin: auto;
+    display: block
+
+`*/
+
+let Wrapper = styled.div`
+    width: 100vw
+`
+
+let Description = styled.div`
+    background-image: url(${storeSrc});
+    width: 60vw;
+    height: 60vh;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 50px auto;
+`
+let DescriptionText = styled.div`
+    height: 200px;
+    background: black;
+    opacity: 75%;
+    color: white;
+    width: 80%;
+    margin: auto;
+    padding: 50px;
+    border-radius: 10px;
+`
+
 export default function Home(){
-    return <h1>Home</h1>
+    return <>
+    <Wrapper>
+    <Description>
+        <DescriptionText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eveniet asperiores excepturi? Odit vero incidunt consequatur aperiam molestiae voluptates beatae, quisquam velit earum accusamus maxime suscipit. Mollitia consequatur illo molestias.
+   </DescriptionText>
+    </Description>
+    <Button as = {Link} to = "/shop">Shop Now</Button>
+    </Wrapper>
+    </>
 }
