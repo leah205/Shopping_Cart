df3b8c6c1cad9c10709986eeeb2ac5200674d131
leah205 <leahtiktin@gmail.com>
2025-06-23 22:44:53 -0600

    set up fake store api fetch request

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,36 @@
 import styled from "styled-components"
 import Item from "./Item"
+import { useEffect, useState } from "react"
+
+const useStoreItems = () => {
+    const [items, setItems ] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+     useEffect(() => {
+        fetch("https://fakestoreapi.com/products", {mode: "cors"})
+        .then(response => {
+            if(response.status >= 400){
+                throw new Error("server error")
+            }
+            return response.json()})
+        .then(response => setItems(response))
+        .catch(error => setError(error))
+        .finally(() => setLoading(false))
+        
+    }, [])
+    return {items, error, loading}
+}
+
 
 const ItemContainer = styled.div`
     display: flex;
     flex-wrap: wrap;`
 export default function Shop(){
+   const {items, error, loading} = useStoreItems();
+   console.log(items)
+
+
+
     return <>
     <h1>Shop</h1>
     <ItemContainer><Item></Item></ItemContainer>
